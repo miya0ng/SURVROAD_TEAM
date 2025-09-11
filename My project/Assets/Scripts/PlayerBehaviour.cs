a39dea13f63544668bfaa1727b230dd8d11e15bf
이승연 <leelaf0002@gmail.com>
2025-09-11 13:47:00 +0900

    플레이어 조작 버튼 구현

---
+++ My project/Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+    private PlayerController playerController;
+    private Rigidbody rb;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    private void Awake()
+    {
+        playerController = GetComponent<PlayerController>();
+        rb = GetComponent<Rigidbody>();
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MoveRotation(rb.rotation * Quaternion.Euler(0f, playerController.hAxis * playerController.rotationSpeed * Time.fixedDeltaTime, 0f));
+        rb.MovePosition(rb.position + transform.forward * playerController.vAxis * playerController.moveSpeed * Time.fixedDeltaTime);
+    }
+}
--- My project/Assets/Scripts/PlayerController.cs
+++ My project/Assets/Scripts/PlayerController.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
 
 //public static class TagManager
@@ -8,34 +9,64 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    public float moveSpeed = 5f;
+    public float vAxis;
+    public float hAxis = 1f;
+    public float moveSpeed = 10f;
     public float rotationSpeed = 180f;
 
-    private float vAxis;
-    private float hAxis;
-    private Rigidbody rb;
-
+    private bool isLeft;
+    private bool isRight;
+    private bool isAccel;
+    private bool isBreak;
 
     private void Awake()
     {
-        rb = GetComponent<Rigidbody>();
+
+    }
+
+    public void ButtonState(UiPlayButton.ButtonType button, bool isheld)
+    {
+        switch(button)
+        {
+            case UiPlayButton.ButtonType.Left:
+                isLeft = isheld;
+                break;
+                case UiPlayButton.ButtonType.Right:
+                isRight = isheld;
+                break;
+            case UiPlayButton.ButtonType.Break:
+                isBreak = isheld;
+                break;
+            case UiPlayButton.ButtonType.Accel:
+                isAccel = isheld;
+                break;
+        }
     }
 
     private void Update()
     {
-        vAxis = Input.GetAxis("Vertical");
-        hAxis = Input.GetAxis("Horizontal");
-    }
+        if (isLeft) 
+        {
+            hAxis = -1f;
+        }
+        if (isRight)
+        {
+            hAxis = 1f;
+        }
+        if (isAccel)
+        {
+            vAxis = 1f;
+            moveSpeed += 1f;
+        }
+        if (isBreak)
+        {
+            moveSpeed -= 2f;
+            vAxis = 0f;
+        }
 
-    private void FixedUpdate()
-    {
-        Debug.Log(vAxis);
-        Debug.Log(hAxis);
-        // 회전
-        rb.MoveRotation(rb.rotation * Quaternion.Euler(0f, hAxis * rotationSpeed * Time.fixedDeltaTime, 0f));
-
-        // 이동
-        rb.MovePosition(rb.position + transform.forward * vAxis * moveSpeed * Time.fixedDeltaTime);
-
+        var curMoveSpeed = Mathf.Clamp(moveSpeed, 0f, 10f);
+        moveSpeed = curMoveSpeed;
+        //vAxis = Input.GetAxis("Vertical");
+        //hAxis = Input.GetAxis("Horizontal");
     }
 }
