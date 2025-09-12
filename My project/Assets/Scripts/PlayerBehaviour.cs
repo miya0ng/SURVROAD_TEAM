23242432f9f87c9f6c0d680585fe6fa8b4e62a4e
이승연 <leelaf0002@gmail.com>
2025-09-12 14:58:42 +0900

    테스트 맵 변경 및, playerController 수치 변경

--- My project/Assets/Scripts/PlayerBehaviour.cs
+++ My project/Assets/Scripts/PlayerBehaviour.cs
@@ -26,8 +26,21 @@ public class PlayerBehaviour : MonoBehaviour
 
     private void FixedUpdate()
     {
-        rb.MoveRotation(rb.rotation * Quaternion.Euler(0f, playerController.hAxis * playerController.rotationSpeed * Time.fixedDeltaTime, 0f));
-        rb.MovePosition(rb.position + transform.forward * playerController.vAxis * playerController.curMoveSpeed * Time.fixedDeltaTime);
+        Move();
         //Debug.Log(playerController.curMoveSpeed);
     }
+
+    private void Move()
+    {
+        //var angles = rb.rotation.eulerAngles;
+        //float currentY = Mathf.DeltaAngle(0, angles.y); // -180 ~ 180
+        //float clampedY = Mathf.Clamp(currentY, 0f, 120f);
+        //Quaternion targetRot = Quaternion.Euler(angles.x, clampedY, angles.z);
+
+        rb.MoveRotation(rb.rotation * Quaternion.Euler(0f, playerController.hAxis * playerController.rotationSpeed, 0f));
+        
+        rb.MovePosition(rb.position + transform.forward * playerController.vAxis * playerController.curMoveSpeed * Time.fixedDeltaTime);
+    }
+
+  
 }
