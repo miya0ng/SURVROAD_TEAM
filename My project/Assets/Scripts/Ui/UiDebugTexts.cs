236b4f22141291e3f97927571d1b724da82aec5c
이승연 <leelaf0002@gmail.com>
2025-09-16 15:19:23 +0900

    플레이어 태그 변경

--- My project/Assets/Scripts/Ui/UiDebugTexts.cs
+++ My project/Assets/Scripts/Ui/UiDebugTexts.cs
@@ -13,8 +13,8 @@ public class UiDebugTexts : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()
     {
-        player = GameObject.FindWithTag("PlayerMe").GetComponent<PlayerController>();
-        playerHp = GameObject.FindWithTag("PlayerMe").GetComponent<PlayerBehaviour>();
+        player = GameObject.FindWithTag("Player").GetComponent<PlayerController>();
+        playerHp = GameObject.FindWithTag("Player").GetComponent<PlayerBehaviour>();
     }
     void Start()
     {
