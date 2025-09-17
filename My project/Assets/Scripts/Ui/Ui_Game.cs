f7c17fa6f295e62d6206b421c72ff6fe61691da7
이승연 <leelaf0002@gmail.com>
2025-09-17 18:00:55 +0900

    무기 슬롯 ui 구현중
    TODO: 장착 무기에 맞는 이미지 연결

--- My project/Assets/Scripts/Ui/Ui_Game.cs
+++ My project/Assets/Scripts/Ui/Ui_Game.cs
@@ -1,14 +1,29 @@
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System.Collections.Generic;
+using NUnit.Framework;
 public class Ui_Game : MonoBehaviour
 {
+    private GameObject player;
     private WaveManager waveManager;
-    public TextMeshProUGUI waveCount;
+    private EquipManager equipManager;
 
+    public TextMeshProUGUI waveCount;
     public TextMeshProUGUI wavePlayTime;
+
+    public TextMeshProUGUI[] slotText;
+    public Image[] slotImage;
+
+    public List<Sprite> loadWeaponThumNails;
+   
+
+    private string[] weaponThumnailName = new string[3]; // 3: equipCount;
+    private int[] weaponSOLevel = new int[3]; //
     public void Awake()
     {
+        player = GameObject.FindWithTag("Player");
+        equipManager = player.GetComponentInChildren<EquipManager>();
         waveManager = GameObject.FindGameObjectWithTag("WaveManager").GetComponent<WaveManager>();
     }
 
@@ -21,5 +36,26 @@ public class Ui_Game : MonoBehaviour
     {
         wavePlayTime.text = $"{waveManager.WaveTimer:F2}";
         waveCount.text = $"{waveManager.currentWave}";
+
+        FindWeaponSO();
+        for (int i = 0; i < equipManager.Slot.Count; i++)
+        {
+            slotText[i].text = "Lv." + weaponSOLevel[i];
+            slotImage[i].sprite = loadWeaponThumNails[i];
+        }
+    }
+
+    private void FindWeaponSO()
+    {
+        if (equipManager.Slot.Count == 0)
+        {
+            return;
+        }
+        for (int i = 0; i < equipManager.Slot.Count; i++)
+        {
+            var w = equipManager.Slot[i].GetComponent<Weapon>();
+            weaponThumnailName[i] = w.weaponSO.PrefabName;
+            weaponSOLevel[i] = w.weaponSO.Level;
+        }
     }
 }
